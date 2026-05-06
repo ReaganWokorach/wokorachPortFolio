b094c5b438ed878aadc439b0a1a1d704b9752526
Reagan Wokorach <106845223+ReaganWokorach@users.noreply.github.com>
2026-05-06 08:04:19 +0300

    Fixing active on scrolls

--- script.js
+++ script.js
@@ -85,3 +85,31 @@ document.querySelectorAll("a").forEach(link => {
   link.setAttribute("target", "_blank");
   link.setAttribute("rel", "noopener noreferrer");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const sections = document.querySelectorAll("section, header");
+  const navLinks = document.querySelectorAll(".nav-links a");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop - 120;
+      const sectionHeight = section.offsetHeight;
+
+      if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach(link => {
+      link.classList.remove("active");
+
+      if (link.getAttribute("href") === "#" + current) {
+        link.classList.add("active");
+      }
+    });
+  });
+
+});
